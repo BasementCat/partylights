641d5424268893f687162b45c36ec4c930a63cf4
Alec Elton <alec.elton@gmail.com>
2020-10-12 17:58:28 -0500

    Cleanup

--- components/lights.py
+++ components/lights.py
@@ -11,17 +11,6 @@ from lib.light.dmx import DMXDevice
 logger = logging.getLogger(__name__)
 
 
-# E_QUIT = (0, "Quit")
-# E_EXCLUSIVE = (1, "Another client is exclusive")
-
-
-# class LightServerClient(ServerClient):
-#     def __init__(self, *args, **kwargs):
-#         super().__init__(*args, **kwargs)
-#         self.monitor = False
-#         self.exclusive = set()
-
-
 class LightOutputTask(Task):
     def setup(self):
         self.lights = {}
@@ -91,68 +80,3 @@ class LightOutputTask(Task):
                     # TODO: try to not receive this, somehow
                     publish('light.state.' + l.name, l.diff_state)
             DMXLight.send_batch(self.dmx_devices, self.lights.values())
-
-    # def _get_lights(self, param, *lights):
-    #     lights = list(filter(None, lights))
-    #     if not lights:
-    #         return self.lights
-    #     out = {}
-    #     for l in lights:
-    #         if l not in self.lights:
-    #             raise RPCError(*RPCError.INVALID_PARAMS, data={'param': param, 'value': l})
-    #         out[l] = self.lights[l]
-    #     return out
-
-    # def cmd_lights(self, client, method, lights=None):
-    #     lights = self._get_lights('lights', *(lights or []))
-    #     return {'result': {name: light.dump() for name, light in lights.items()}}
-
-    # def cmd_state(self, client, method, lights=None, state=None):
-    #     lights = self._get_lights('lights', *(lights or []))
-    #     light_names = set(lights.keys())
-
-    #     for cl in self.server.clients.values():
-    #         if cl.exclusive and cl is not client:
-    #             failed = light_names & cl.exclusive
-    #             if failed:
-    #                 raise RPCError(*E_EXCLUSIVE, data={'lights': list(failed)})
-
-    #     if state:
-    #         out = {}
-    #         for light in lights.values():
-    #             light.set_state(**state)
-    #             out[light.name] = light.diff_state
-    #         self.rpcserver.send(lambda cl: cl.monitor and cl is not client, {'result': out})
-    #         # The requesting client must get the results as a reply
-    #         return {'result': {'type': 'state', 'data': out}}
-    #     else:
-    #         out = {l.name: l.state for l in lights.values()}
-    #         return {'result': out}
-
-    # def cmd_monitor(self, client, method, state=None):
-    #     if state is not None:
-    #         client.monitor = bool(state)
-    #     return {'state': client.monitor}
-
-    # def cmd_exclusive(self, client, method, state=None, lights=None):
-    #     if state is not None:
-    #         state = bool(state)
-    #         light_names = set(self._get_lights('lights', *(lights or [])).keys())
-    #         if state:
-    #             # Can't go exclusive if any other clients are
-    #             for cl in self.server.clients.values():
-    #                 if cl.exclusive and cl is not client:
-    #                     # Determine if this client can gain exclusivity on the lights requested
-    #                     failed = cl.exclusive & light_names
-    #                     if failed:
-    #                         raise RPCError(*E_EXCLUSIVE, data={'lights': list(failed)})
-    #             # Not failed at this point, add the list of lights to the client
-    #             client.exclusive |= light_names
-    #         else:
-    #             client.exclusive -= light_names
-
-    #     return {
-    #         'num': len(client.exclusive),
-    #         'all': client.exclusive == set(self.lights.keys()),
-    #         'lights': list(client.exclusive),
-    #     }
